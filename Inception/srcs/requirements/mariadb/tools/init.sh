76c70d247cb78d8ba91d1abb4a170c76f52b2957
Joan Renau valls <jrenau-v@car10s1.42barcelona.com>
2024-10-10 19:37:53 +0200

    Made some envs

--- Inception/srcs/requirements/mariadb/tools/init.sh
+++ Inception/srcs/requirements/mariadb/tools/init.sh
@@ -3,8 +3,8 @@
 /etc/init.d/mariadb start
 chown mysql:mysql /var/lib/mysql
 cp -r /sdaf /var/lib/mysql
-mariadb --execute "create database wordpress;"
-mariadb --execute "grant all privileges on wordpress.* to uwordpress identified by 'securePass';"
+mariadb --execute "create database  ${DATABASE_NAME};"
+mariadb --execute "grant all privileges on ${DATABASE_NAME}.* to ${DATABASE_USER} identified by 'securePass';"
 mariadb --execute "flush privileges"
 /etc/init.d/mariadb stop
 sleep 5
--- Inception/srcs/requirements/wordpress/tools/init.sh
+++ Inception/srcs/requirements/wordpress/tools/init.sh
@@ -3,9 +3,10 @@
 sleep 10
 wp core download --allow-root --path=wordpress
 cd wordpress
-wp config create --allow-root --dbname=wordpress --dbuser=uwordpress --dbhost=mariadb --dbpass=securePass --prompt=securePass
-wp core install --allow-root --url=jrenau-v.42.fr --title="twiki" --admin_user=wpadmin --admin_password=wpadminpass --admin_email=real@realer.realest
-wp user create "sdaf" "sdaf@fasd.com" --role=author --user_pass="sdaf" --path=/var/www/html/wordpress --allow-root
+wp config create --allow-root --dbname=${DATABASE_NAME} --dbuser=${DATABASE_USER} --dbhost=mariadb --dbpass=securePass --prompt=securePass
+#wp core install --allow-root --url=jrenau-v.42.fr --title="twiki" --admin_user=wpadmin --admin_password=wpadminpass --admin_email=real@realer.realest
+wp core install --allow-root --url=${HOST_NAME} --title=${WP_ADMIN_TITLE} --admin_user=${WP_ADMIN_USR} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_MAIL}
+wp user create ${WP_USER_USR} ${WP_USER_MAIL} --role=author --user_pass=${WP_USER_PASS} --path=/var/www/html/wordpress --allow-root
 
 chown -R root:hosttwikituki ./
 chmod -R 775 ./
