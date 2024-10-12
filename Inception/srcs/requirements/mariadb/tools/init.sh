e7498e667726866b6d3bb137b77560b0ab7d9d8b
Joan Renau valls <jrenau-v@car1s2.42barcelona.com>
2024-10-12 18:48:07 +0200

    Works all good

--- Inception/srcs/requirements/mariadb/tools/init.sh
+++ Inception/srcs/requirements/mariadb/tools/init.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
 if [ ! -d /var/lib/mysql/wordpress ] ; then 
 	/etc/init.d/mariadb start
-	chown mysql:mysql /var/lib/mysql
-	cp -r /sdaf /var/lib/mysql
 	mariadb --execute "create database  ${DATABASE_NAME};"
 	mariadb --execute "grant all privileges on ${DATABASE_NAME}.* to ${DATABASE_USER} identified by 'securePass';"
 	mariadb --execute "flush privileges"
 	/etc/init.d/mariadb stop
 	sleep 5
-	rm /run/mysqld/mysqld.sock
 fi
-
 mariadbd
-echo '::::::::::::::::: tailing :( :::::::::::::::::'
-#tail -f
